d7d0efefc859b251aa18feb292102aa006e89f93
Dipakshi07 <dipakshiojha@gmail.com>
2026-02-04 13:32:54 +0530

    Refactor MealCard to improve navigation and styling
    Refactor MealCard component to use useNavigate for routing and added CSS styles.

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./MealCard.css";
 
-const MealCard = ({detail}) => {
-    console.log(detail);
-    
+const MealCard = ({ detail }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className='meals w-full h-fit flex flex-wrap gap-10 justify-center items-center p-10'>
-        {!detail ? "No data found": detail.map((item) =>{
-           return(
-              <div className='meal-card w-[250px] h-[350px] border-2 border-white bg-gray-300 rounded-lg flex flex-col justify-center items-center gap-5'>
-                 <img className='w-[200px] h-[200px] rounded' src={item.strMealThumb}/>
-                 <p className='text-center font-bold'>{item.strMeal}</p>
-                 <NavLink to={`/${item.idMeal}`}>
-                    <button className='bg-rose-300 px-10 py-1 rounded-lg font-semibold shadow-xl shadow-black '>Recipe</button>
-                  </NavLink>
-           </div>
-           )
-        })
-        }
-
+    <div className="meals">
+      {!detail
+        ? "No data found"
+        : detail.map((item) => {
+            return (
+              <div
+                className="meal-card"
+                key={item.idMeal}
+                onClick={() => navigate(`/${item.idMeal}`)}
+              >
+                <img src={item.strMealThumb} alt={item.strMeal} />
+                <p>{item.strMeal}</p>
+                <button>View Recipe</button>
+              </div>
+            );
+          })}
     </div>
-  )
-}
+  );
+};
 
-export default MealCard
+export default MealCard;
